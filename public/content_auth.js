2510b336554ddf5701dace15ca145de0a61a166d
Detellez <164583101+Detellez@users.noreply.github.com>
2026-04-08 14:58:00 -0400

    Update content_auth.js

--- public/content_auth.js
+++ public/content_auth.js
@@ -628,8 +628,8 @@
     // ==========================================
     // 🌐 EL ENRUTADOR INTELIGENTE V12
     // ==========================================
-    const CEREBRO_URL = 'https://script.google.com/macros/s/AKfycbz5bBK7rCjvovEXg6WbbWoe3YwNf041QITDQOKendc-qNGu_pP1YkrSXklQNkKPcoGXXg/exec';
-    const FIREBASE_URL = 'https://notificacionlore-default-rtdb.firebaseio.com/alerta_activa.json'; // 🔥 MOVIDO AQUÍ ARRIBA
+    const CEREBRO_URL = 'https://script.google.com/macros/s/AKfycbx2MmJpsF1jgwyhmH4AuYpOoRQKv4U6AEo9HQiDv7LxXx8TR3qNHFLczu1TyCMvCAsl/exec';
+    const FIREBASE_URL = 'https://notificacionalt0-default-rtdb.firebaseio.com'; // 🔥 MOVIDO AQUÍ ARRIBA
     const API_URL = CEREBRO_URL;
     
     // Variable para detener intervalos
